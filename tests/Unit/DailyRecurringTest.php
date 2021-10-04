020a47fcdd312a3e5a8bed4c5560d6b8340e0f5c
Michal Kortas <michal@webroad.pl>
2021-10-04 13:15:59 +0200

    Update tests

--- tests/Unit/DailyRecurringTest.php
+++ tests/Unit/DailyRecurringTest.php
@@ -72,3 +72,59 @@ test('it return origin of daily event', function () {
 
     expect($originArray)->toEqual($eventArray);
 });
+
+test('it recurring only on specified day', function () {
+    $eventFrom = new DateTime('wednesday this week');
+    $eventTo = (new DateTime('wednesday this week'))->modify('+2 hours');
+    $stopRecurring = (new DateTime('wednesday this week'))->modify('+14 days');
+
+    $event = new CalendarEvent(
+        $eventFrom->format('Y-m-d H:i'),
+        $eventTo->format('Y-m-d H:i')
+    );
+
+    $recurring = new DailyRecurring($event, $stopRecurring, 1, [1]);
+
+    expect($recurring->getDays())->toHaveCount(2);
+});
+
+test('it recurring only on specified days', function () {
+    $eventFrom = new DateTime('wednesday this week');
+    $eventTo = (new DateTime('wednesday this week'))->modify('+2 hours');
+    $stopRecurring = (new DateTime('wednesday this week'))->modify('+14 days');
+
+    $event = new CalendarEvent(
+        $eventFrom->format('Y-m-d H:i'),
+        $eventTo->format('Y-m-d H:i')
+    );
+
+    $recurring = new DailyRecurring($event, $stopRecurring, 1, [1, 5]);
+
+    expect($recurring->getDays())->toHaveCount(4);
+});
+
+test('it return origin of weekly event', function () {
+    $eventFrom = new DateTime('wednesday this week');
+    $eventTo = (new DateTime('wednesday this week'))->modify('+2 hours');
+    $stopRecurring = (new DateTime('wednesday this week'))->modify('+14 days');
+
+    $event = new CalendarEvent(
+        $eventFrom->format('Y-m-d H:i'),
+        $eventTo->format('Y-m-d H:i')
+    );
+
+    $recurring = new DailyRecurring($event, $stopRecurring, 1);
+
+    $originArray = [
+        $recurring->getOrigin()->from->format('YmdHis'),
+        $recurring->getOrigin()->to->format('YmdHis'),
+    ];
+
+    $eventArray = [
+        $eventFrom->format('YmdHis'),
+        $eventTo->format('YmdHis'),
+    ];
+
+    expect($originArray)->toEqual($eventArray);
+});
+
